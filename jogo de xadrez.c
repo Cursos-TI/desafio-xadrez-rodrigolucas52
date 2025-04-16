a4ba81d60064d1643586a47a8f35889ad5ce2835
rodrigolucas52 <rodrigo.lucas52@gmail.com>
2025-04-15 22:10:12 -0300

    jogo de xadrez.c

--- jogo de xadrez.c
+++ jogo de xadrez.c
@@ -2,8 +2,8 @@
 
 int main(){
 int i = 1; 
-int j = 1; // Inicializa a variável j com 1 p/ rainha
-     // Inicializa a variável i com 1 p/ bispo
+int j = 1; // Inicializa a variável j com 1 p/ rainha     // Inicializa a variável i com 1 p/ bispo
+int movimentoCavalo = 1; // para movimento do cavalo em L.
 // while para bispo,  do while rainha, e for torre.
 
 for ( int i = 0; i < 5; i++){
@@ -14,13 +14,24 @@ while (i <= 5) { // Enquanto i for menor ou igual a 5, o loop continua
     printf("Diagonal cima direita Bispo\n"); // imprime a direcao do movimento do bispo
     i++; // Incrementa i em 1 a cada iteração " i++ o que ele faz pega o valor de i e incrementa + 1"
 
- }
-    
+ }    
 
     do {
         printf("Movimento para esquerda, rainha\n", i); // imprime a direcao do movimento da rainha
         j++; // Incrementa i em 1 a cada iteração " i++ o que ele faz pega o valor de i e incrementa + 1"
     } while (j <= 8); // O loop do-while executa enquanto a condição for verdadeira
 
+
+        while (movimentoCavalo--)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                printf("Cavalo para Baixo\n"); // imprime a direcao do movimento do cavalo
+            }
+            printf("Cavalo para Esquerda\n"); // imprime a direcao do movimento do cavalo
+        }
+
+        
     return 0;
+
 }
